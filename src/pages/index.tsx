c627d1a089063f4dbf26ba3b79e1bddeee506a3a
mirupapa <mirutiropapa@icloud.com>
2021-03-21 16:27:46 +0900

    とりみんぐ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { Box, Heading } from '@chakra-ui/react'
 import React from 'react'
+import Editor from 'src/components/Editor'
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <Box>
-      <Heading>Hello World!</Heading>
+      <Heading>Image Editor</Heading>
+      <Editor />
     </Box>
   )
 }
+
+export default Home
